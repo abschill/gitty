fd72b4089cd6cb1a259fe4827aac9582b75f1251
abschill <ajay198810@gmail.com>
2022-04-19 15:54:10 -0600

    fix merge

--- src/merge-to.sh
+++ src/merge-to.sh
@@ -4,9 +4,9 @@ to_branchname=$1
 [[ -z $to_branchname ]] &&echo -e "error: \e[31mspecify a branch" && exit
 
 # in this example im running it from the dir outside src
-br=$(./src/get-branch.sh)
+br=$(git branch)
 
 [[ $br -ne "master" ]] && git checkout $to_branchname && git merge $br && exit
 
-[[ $br -eq "master" ]] && echo -e "error: \e[31mYou're already on master"
+echo -e "error: \e[31mYou're on master"
 #
